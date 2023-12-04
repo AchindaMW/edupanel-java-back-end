beb62c206b43f8c7fc2fca67cf68a0f89abddc18
Achinda <achindamihiruk@gmail.com>
2023-12-04 19:05:03 +0530

    Create newLecturer WS

--- src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,5 +1,10 @@
 package lk.ijse.dep11.edupanel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +12,27 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebAppConfig {
+
+    @Bean
+    public Bucket defaultBucket() throws IOException {
+        InputStream serviceAccount =
+                getClass().getResourceAsStream("/edupanel-d6b68-firebase-adminsdk-9gpmn-76c37c38b9.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("edupanel-d6b68.appspot.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        return StorageClient.getInstance().bucket();
+    }
 
     @Bean
     public StandardServletMultipartResolver multipartResolver(){
--- src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
@@ -1,5 +1,10 @@
 package lk.ijse.dep11.edupanel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
@@ -7,15 +12,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class WebRootConfig {
-    @Bean
+
+    @Bean(destroyMethod = "close") // destroy pool after shutdown
     public HikariDataSource dataSource(Environment env){
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
         config.setUsername(env.getRequiredProperty("spring.datasource.username"));
-        config.setUsername(env.resolvePlaceholders("spring.datasource.password"));
+        config.setPassword(env.getRequiredProperty("spring.datasource.password"));
         config.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
         config.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari.maximum-pool-size", Integer.class));
         return new HikariDataSource(config);
