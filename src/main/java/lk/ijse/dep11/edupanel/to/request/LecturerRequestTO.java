1a143d60a5ea0b35255497505ea3b85f5069321e
Achinda <achindamihiruk@gmail.com>
2023-12-04 18:10:13 +0530

    Implement createNewLecturer

--- src/main/java/lk/ijse/dep11/edupanel/to/request/LecturerRequestTO.java
+++ src/main/java/lk/ijse/dep11/edupanel/to/request/LecturerRequestTO.java
@@ -18,17 +18,22 @@ public class LecturerRequestTO implements Serializable {
     @NotBlank(message = "Name cannot be empty")
     @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid name: {value}")
     private String name;
+
     @NotBlank(message = "Designation cannot be empty")
     @Length(min = 2, message = "Invalid designation: {value}")
     private String designation;
+
     @NotBlank(message = "Qualifications cannot be empty")
     @Length(min = 2, message = "Invalid qualifications: {value}")
     private String qualifications;
+
     @NotBlank(message = "Type cannot be empty")
     @Pattern(regexp = "^(full-time|part-time)$", flags = Pattern.Flag.CASE_INSENSITIVE,message = "Invalid type")
     private String type;
+
     @LecturerImage
     private MultipartFile picture;
+
     @Pattern(regexp = "^http[s]?://.+$", message = "Invalid linkedin url")
     private String linkedin;
 }
